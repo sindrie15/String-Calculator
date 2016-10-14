18fdacb2aaf2267d8a1e9c328ceb3eb0318ab75b
sindrie15 <sindrie15@ru.is>
2016-10-14 13:24:19 +0000

    Private helpers

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,15 +6,25 @@ public class Calculator {
 			if(text.equals(""))
 				return 0;
 			else if (text.contains(",")){
-				String [] numbers = text.split(",");
-				return toInt(numbers[0]) + toInt(numbers[1]);
+				return tsum(splitNumbers(text));
 			}
 			else
-				return 1;
-		
+				return 1;		
 	}
 
 	private static int toInt(String text){
 		return Integer.parseInt(text);
+	}
+
+	private static String[] splitNumbers(String numbers){
+		return numbers.split(",");
+	}
+
+	private static int sum(String[] numbers){
+		int total = 0;
+		for(String number : numbers){
+			total += toInt(number);
+		}
+		return total;
 	}
 }
